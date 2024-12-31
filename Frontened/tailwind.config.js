47fd82789efd124beb1e78973473e79a8bd299bc
Nikhil Kumar <nikhil20092003@gmail.com>
2024-12-31 21:53:02 +0530

    initial commit

---
+++ Frontened/tailwind.config.js
@@ -0,0 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   darkMode:"class",
+
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+// }
+
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
