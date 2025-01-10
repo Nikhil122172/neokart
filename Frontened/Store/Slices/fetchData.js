bad90fd7139b681e2648d8527403afad8248e4a4
Nikhil Kumar <nikhil20092003@gmail.com>
2025-01-10 19:18:30 +0530

    adding backend url

--- Frontened/Store/Slices/fetchData.js
+++ Frontened/Store/Slices/fetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const FETCH = "FETCH DATA SUCCESSFUL"
 
 const fetchProdduct = () => async dispatch =>{
-    const response = await axios.get("http://localhost:4001/api/adminData");
+    const response = await axios.get("https://neokartb2.onrender.com/api/adminData");
     console.log("ye fetched data hai18: ",response.data);
     
     dispatch({type: FETCH, payload: response.data});
